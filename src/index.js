4c5b3590c90f7db46d47f15c78e47979bdc2687b
kilokid <arlomov007@gmail.com>
2021-12-22 12:49:50 +0300

    feat: initial markup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './components/app/app';
 
 ReactDOM.render(
   <StrictMode>
-    
+    <App />
   </StrictMode>,
   document.getElementById('root')
 );
