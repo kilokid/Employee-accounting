41e99175fe3b42e5a534faea85f9646f6df5a8e2
kilokid <arlomov007@gmail.com>
2021-12-22 12:56:57 +0300

    feat: app-info component & his styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,14 @@
+import AppInfo from '../app-info/app-info';
+
 import './app.css';
 
-function App() {
+
+const App = () => {
     return (
         <div className="app">
-
+            <AppInfo />
         </div>
     );
-}
+};
 
 export default App;
